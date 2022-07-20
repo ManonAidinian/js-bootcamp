04f367107596013fed02bdab91669c1e0378ae73
manonaidinian <manonaidinian@gmail.com>
2022-07-20 20:59:08 +0800

    event listener

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,23 +1,73 @@
-console.log('hello from notes-app.js')
+const notes =[{
+  title: 'My next trip',
+  body: 'Visit Sihka'
+}, {
+  title: 'Good habits',
+  body: 'Stop smoking'
+}, {
+  title: 'Important',
+  body: 'Find a job'
+}]
 
-// ----------------query and remove (first element) ---------------
 
-const p = document.querySelector('p')
-//p for paragraph tag
-p.remove()
+// ----------------input ---------------
 
-
-// ----------------query all and remove---------------
-
-
-const ps = document.querySelectorAll('p')
-//p for paragraph tag
-ps.forEach(p => {
-  p.remove()
+document.querySelector('#search').addEventListener('input', (event) => {
+  filters.searchText = event.target.value
+  renderNotes(notes, filters)
 })
 
-// ----------------query and read in console and modify !---------------
+// ----------------filtering from input---------------
 
-const h1 = document.querySelector('h1')
-console.log(h1.textContent)
-h1.textContent = 'Surprise!!!'
+const filters = {
+  searchText: ''
+}
+
+const renderNotes = (notes, filters) => {
+  const filteredNotes = notes.filter(note => {
+    return note.title.includes(filters.searchText)
+  })
+  document.querySelector('#notes').innerHTML = ''
+  filteredNotes.forEach(note => {
+    const noteElement = document.createElement('p')
+    noteElement.textContent = note.title
+    document.querySelector('#notes').appendChild(noteElement)
+  })
+}
+renderNotes(notes, filters)
+
+// ----------------buttons manipulations ---------------
+
+document.querySelector('#create-new').addEventListener('click', (event) => {
+  event.target.textContent = 'Bravo!'
+})
+
+document.querySelector('#remove-all').addEventListener('click', (event) => {
+  document.querySelectorAll('.note').forEach(note => note.remove())
+})
+
+// // ----------------query and remove (first element) ---------------
+
+// const p = document.querySelector('p')
+// //p for paragraph tag
+// p.remove()
+
+// // ----------------query all and remove---------------
+
+// const ps = document.querySelectorAll('p')
+// //p for paragraph tag
+// ps.forEach(p => {
+//   p.remove()
+// })
+
+// // ----------------query and read in console and modify !---------------
+
+// const h1 = document.querySelector('h1')
+// console.log(h1.textContent)
+// h1.textContent = 'Surprise!!!'
+
+// // ----------------add a new element---------------
+
+// const newParagraph = document.createElement('p')
+// newParagraph.textContent = 'This is a new element from JavaScript'
+// document.querySelector('body').appendChild(newParagraph)
--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -1,6 +1,51 @@
-const ps = document.querySelectorAll('p')
-ps.forEach(p => {
-if (p.textContent.includes('the')) {
-  p.remove()
-}
+const todos = [{
+  text: 'laundery',
+  completed: true
+}, {
+  text: 'groceries',
+  completed: false
+}, {
+  text: 'waxing',
+  completed: true
+}, {
+  text: 'honda',
+  completed: false
+}, {
+  text: 'cat food',
+  completed: false
+}]
+
+document.querySelector('#new-todo').addEventListener('input', (event) => {
+  console.log(event.target.value)
 })
+
+
+
+const incomplete = todos.filter(todo => !todo.completed)
+
+const summary = document.createElement('h3')
+summary.textContent = `You have ${incomplete.length} todos left`
+document.querySelector('body').appendChild(summary)
+
+
+incomplete.forEach(todoobject => {
+  const todo = document.createElement('p')
+  todo.textContent = todoobject.text
+  document.querySelector('body').appendChild(todo)
+})
+
+document.querySelector('#click-me').addEventListener('click', (event) =>{
+  event.target.textContent = 'Bravo !'
+  const msg = document.createElement('p')
+  msg.textContent = 'You can add a new to do'
+  document.querySelector('body').appendChild(msg)
+})
+
+
+// ------------------  select and remove based on condition --------------------
+// const ps = document.querySelectorAll('p')
+// ps.forEach(p => {
+// if (p.textContent.includes('the')) {
+//   p.remove()
+// }
+// })
