419fa7baf78c2364f78ae2437af8abd2b4344d35
manonaidinian <manonaidinian@gmail.com>
2022-07-28 16:51:42 +0800

    javascript in the browser

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,50 +1,78 @@
-const notes =[{
-  title: 'My next trip',
-  body: 'Visit Sihka'
-}, {
-  title: 'Good habits',
-  body: 'Stop smoking'
-}, {
-  title: 'Important',
-  body: 'Find a job'
-}]
+// ➜  js-bootcamp git:(master) live-server notes-app
 
+const notes = [
+  {
+    title: "My next trip",
+    body: "Visit Sihka",
+  },
+  {
+    title: "Good habits",
+    body: "Stop smoking",
+  },
+  {
+    title: "Important",
+    body: "Find a job",
+  },
+];
 
 // ----------------input ---------------
 
-document.querySelector('#search').addEventListener('input', (event) => {
-  filters.searchText = event.target.value
-  renderNotes(notes, filters)
-})
+document.querySelector("#search").addEventListener("input", (event) => {
+  filters.searchText = event.target.value;
+  renderNotes(notes, filters);
+});
 
 // ----------------filtering from input---------------
 
 const filters = {
-  searchText: ''
-}
+  searchText: "",
+};
 
 const renderNotes = (notes, filters) => {
-  const filteredNotes = notes.filter(note => {
-    return note.title.includes(filters.searchText)
-  })
-  document.querySelector('#notes').innerHTML = ''
-  filteredNotes.forEach(note => {
-    const noteElement = document.createElement('p')
-    noteElement.textContent = note.title
-    document.querySelector('#notes').appendChild(noteElement)
-  })
-}
-renderNotes(notes, filters)
+  const filteredNotes = notes.filter((note) => {
+    return note.title.includes(filters.searchText);
+  });
+  document.querySelector("#notes").innerHTML = "";
+  filteredNotes.forEach((note) => {
+    const noteElement = document.createElement("p");
+    noteElement.textContent = note.title;
+    document.querySelector("#notes").appendChild(noteElement);
+  });
+};
+renderNotes(notes, filters);
 
 // ----------------buttons manipulations ---------------
 
-document.querySelector('#create-new').addEventListener('click', (event) => {
-  event.target.textContent = 'Bravo!'
-})
+document.querySelector("#create-new").addEventListener("click", (event) => {
+  event.target.textContent = "Bravo!";
+});
 
-document.querySelector('#remove-all').addEventListener('click', (event) => {
-  document.querySelectorAll('.note').forEach(note => note.remove())
-})
+// ----------------dropdown manipulations ---------------
+
+document.querySelector("#filter-by").addEventListener("change", (event) => {
+  event.target.value;
+  console.log(event.target.value);
+});
+
+// ----------------checkbox manipulations ---------------
+
+// document.querySelector("#for-fun").addEventListener("change", (event) => {
+//   event.target.checked;
+// });
+
+// // ----------------form event handeler ---------------
+
+// document.querySelector("#name-form").addEventListener("submit", (event) => {
+//   event.preventDefault();
+//   console.log(event.target.elements.firstName.value);
+//   event.target.elements.firstName.value = "";
+// });
+
+// // ----------------remove noted ---------------
+
+// document.querySelector("#remove-all").addEventListener("click", (event) => {
+//   document.querySelectorAll(".note").forEach((note) => note.remove());
+// });
 
 // // ----------------query and remove (first element) ---------------
 
--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -1,48 +1,103 @@
-const todos = [{
-  text: 'laundery',
-  completed: true
-}, {
-  text: 'groceries',
-  completed: false
-}, {
-  text: 'waxing',
-  completed: true
-}, {
-  text: 'honda',
-  completed: false
-}, {
-  text: 'cat food',
-  completed: false
-}]
+// ➜  js-bootcamp git:(master) live-server todo-app
 
-document.querySelector('#new-todo').addEventListener('input', (event) => {
-  console.log(event.target.value)
-})
+const todos = [
+  {
+    text: "laundery",
+    completed: true,
+  },
+  {
+    text: "groceries",
+    completed: false,
+  },
+  {
+    text: "waxing",
+    completed: true,
+  },
+  {
+    text: "honda",
+    completed: false,
+  },
+  {
+    text: "cat food",
+    completed: false,
+  },
+];
 
+// -------------------Checkbox-------------------------------
 
+document
+  .querySelector("#hide-completed")
+  .addEventListener("change", (event) => {
+    filters.hideCompleted = event.target.checked;
+    renderTodos(todos, filters);
+  });
 
-const incomplete = todos.filter(todo => !todo.completed)
+// -------------------Add to do with form--------------------
 
-const summary = document.createElement('h3')
-summary.textContent = `You have ${incomplete.length} todos left`
-document.querySelector('body').appendChild(summary)
+document.querySelector("#todo-form").addEventListener("submit", (event) => {
+  event.preventDefault();
+  todos.push({
+    text: event.target.elements.todo.value,
+    completed: false,
+  });
+  renderTodos(todos, filters);
+  event.target.elements.todo.value = "";
+});
 
+// -------------------Add to do with input and button--------------------
 
-incomplete.forEach(todoobject => {
-  const todo = document.createElement('p')
-  todo.textContent = todoobject.text
-  document.querySelector('body').appendChild(todo)
-})
+// let newToDo;
 
-document.querySelector('#click-me').addEventListener('click', (event) =>{
-  event.target.textContent = 'Bravo !'
-  const msg = document.createElement('p')
-  msg.textContent = 'You can add a new to do'
-  document.querySelector('body').appendChild(msg)
-})
+// document.querySelector("#new-todo").addEventListener("input", (event) => {
+//   newToDo = event.target.value;
+// });
+// // note: inout track individual keystroke
 
+// document.querySelector("#click-me").addEventListener("click", (event) => {
+//   event.target.textContent = "Bravo !";
+//   const msg = document.createElement("p");
+//   msg.textContent = newToDo;
+//   document.querySelector("body").appendChild(msg);
+// });
+
+// -------------------Filter and render to do--------------------
+
+const filters = {
+  searchText: "",
+  hideCompleted: false,
+};
+
+const renderTodos = (todos, filter) => {
+  const filteredTodos = todos.filter((todo) => {
+    const searchTextMatch = todo.text.includes(filter.searchText);
+    const hideCompletedMatch = !filters.hideCompleted || !todo.completed;
+    return searchTextMatch && hideCompletedMatch;
+  });
+
+  const incomplete = filteredTodos.filter((todo) => !todo.completed);
+
+  document.querySelector("#todos").innerHTML = "";
+
+  const summary = document.createElement("h3");
+  summary.textContent = `You have ${incomplete.length} todos left`;
+  document.querySelector("#todos").appendChild(summary);
+
+  filteredTodos.forEach((todoobject) => {
+    const todo = document.createElement("p");
+    todo.textContent = todoobject.text;
+    document.querySelector("#todos").appendChild(todo);
+  });
+};
+
+renderTodos(todos, filters);
+
+document.querySelector("#searchtodo").addEventListener("input", (event) => {
+  filters.searchText = event.target.value;
+  renderTodos(todos, filters);
+});
 
 // ------------------  select and remove based on condition --------------------
+
 // const ps = document.querySelectorAll('p')
 // ps.forEach(p => {
 // if (p.textContent.includes('the')) {
