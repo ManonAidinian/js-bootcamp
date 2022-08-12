16210d4e6c8d1177d7deb7256d9888da38766c04
manonaidinian <manonaidinian@gmail.com>
2022-08-12 13:34:06 +0800

    type coercion

---
+++ advanced/type-coercion.js
@@ -0,0 +1,11 @@
+console.log("5" + 5); //interpretate as string => 55
+console.log("5" - 5); //interpretate as number => 0
+console.log("5" === 5); // false
+console.log("5" == 5); // true (does not take type in count)
+
+console.log(typeof 123);
+
+const value = true + 12;
+const type = typeof value;
+console.log(value);
+console.log(type);
