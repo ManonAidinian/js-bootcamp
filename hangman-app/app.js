a28ebd4663cc61f6b0e0e5f83aec382352f9cd5b
manonaidinian <manonaidinian@gmail.com>
2022-08-21 09:21:53 +0800

    prototype

--- hangman-app/app.js
+++ hangman-app/app.js
@@ -1,10 +1,8 @@
-const product = {
-  name: "Sapiens",
-};
+// Primitive value: doesnt not have properties
+// String, Number, Boolen, Null, Undefined
+// Anything else is an Object (Object, Array, Function...)
+const number = 33;
+console.log(number);
 
-console.log(product.hasOwnProperty("name"));
-Object.prototype.hasOwnProperty = () => "new function";
-console.log(product.hasOwnProperty());
-
-Object.prototype.someNewMethod = () => "whatever";
-console.log(product.someNewMethod());
+const otherNumber = new Number(34);
+console.log(otherNumber);
