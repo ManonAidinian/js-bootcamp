1116dd6e855d1eee524002691d388f810e89fc0d
manonaidinian <manonaidinian@gmail.com>
2022-08-26 16:51:32 +0800

    changing syntax to class

--- hangman-app/hangman.js
+++ hangman-app/hangman.js
@@ -1,63 +1,62 @@
-const HangMan = function (word, remainingGuesses) {
-  this.word = word.toLowerCase().split("");
-  this.remainingGuesses = remainingGuesses;
-  this.guessedLetters = [];
-  this.status = "playing";
-};
-
-HangMan.prototype.calculateStatus = function () {
-  let finished = true;
-  this.word.forEach((letter) => {
-    if (this.guessedLetters.includes(letter)) {
-    } else {
-      finished = false;
-    }
-  });
-
-  if (this.remainingGuesses === 0) {
-    this.status = "failed";
-  } else if (finished) {
-    this.status = "finished";
-  } else {
+class HangMan {
+  constructor(word, remainingGuesses) {
+    this.word = word.toLowerCase().split("");
+    this.remainingGuesses = remainingGuesses;
+    this.guessedLetters = [];
     this.status = "playing";
   }
-};
 
-HangMan.prototype.getStatusMessage = function () {
-  if (this.status === "playing") {
-    return `Guesses left: ${this.remainingGuesses}`;
-  } else if (this.status === "failed") {
-    return `Nice try, the word was "${this.word.join("")}"`;
-  } else {
-    return `Great work! You guessed the word`;
-  }
-};
-
-HangMan.prototype.getPuzzle = function () {
-  let puzzle = "";
-  this.word.forEach((letter) => {
-    if (this.guessedLetters.includes(letter) || letter === " ") {
-      puzzle += letter;
+  calculateStatus() {
+    let finished = true;
+    this.word.forEach((letter) => {
+      if (this.guessedLetters.includes(letter)) {
+      } else {
+        finished = false;
+      }
+    });
+    if (this.remainingGuesses === 0) {
+      this.status = "failed";
+    } else if (finished) {
+      this.status = "finished";
     } else {
-      puzzle += "*";
+      this.status = "playing";
     }
-  });
-  return puzzle;
-};
-
-HangMan.prototype.makeGuess = function (guessedLetter) {
-  guessedLetter = guessedLetter.toLowerCase();
-  const exist = this.guessedLetters.includes(guessedLetter);
-
-  if (this.status !== "playing") {
-    return;
   }
 
-  if (!exist) {
-    this.guessedLetters.push(guessedLetter);
+  getStatusMessage() {
+    if (this.status === "playing") {
+      return `Guesses left: ${this.remainingGuesses}`;
+    } else if (this.status === "failed") {
+      return `Nice try, the word was "${this.word.join("")}"`;
+    } else {
+      return `Great work! You guessed the word`;
+    }
   }
-  if (!exist && !this.word.includes(guessedLetter)) {
-    this.remainingGuesses--;
+
+  getPuzzle() {
+    let puzzle = "";
+    this.word.forEach((letter) => {
+      if (this.guessedLetters.includes(letter) || letter === " ") {
+        puzzle += letter;
+      } else {
+        puzzle += "*";
+      }
+    });
+    return puzzle;
   }
-  this.calculateStatus();
-};
+
+  makeGuess(guessedLetter) {
+    guessedLetter = guessedLetter.toLowerCase();
+    const exist = this.guessedLetters.includes(guessedLetter);
+    if (this.status !== "playing") {
+      return;
+    }
+    if (!exist) {
+      this.guessedLetters.push(guessedLetter);
+    }
+    if (!exist && !this.word.includes(guessedLetter)) {
+      this.remainingGuesses--;
+    }
+    this.calculateStatus();
+  }
+}
--- oop/person.js
+++ oop/person.js
@@ -1,5 +1,37 @@
 // Prototypal inheritance
 
+class PersonClass {
+  constructor(firstName, lastName, age, likes = []) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.likes = likes;
+  }
+  getBio() {
+    let bio = `${this.firstName} is ${this.age}.`;
+    this.likes.forEach((like) => {
+      bio += ` ${this.firstName} likes ${like}.`;
+    });
+    // this function child HAS to be arrow function but still have access to this from its parent.
+    return bio;
+  }
+  changeName(fullName) {
+    const names = fullName.split(" ");
+    this.firstName = names[0];
+    this.lastName = names[1];
+  }
+}
+
+const myPerson = new PersonClass("Manon", "Aidi", 34, ["my cats"]);
+console.log(myPerson);
+console.log(myPerson.getBio());
+
+//
+//
+// -----------------------OLD SYNTAX--------------------------------
+//
+//
+
 const Person = function (firstName, lastName, age, likes = []) {
   this.firstName = firstName;
   this.lastName = lastName;
@@ -26,7 +58,7 @@ Person.prototype.changeName = function (fullName) {
 
 const me = new Person("Manon", "Aidinian", 34, ["cat", "surf"]);
 me.changeName("Agathe Mahé");
-console.log(me.getBio());
+// console.log(me.getBio());
 
 const other = new Person("Guido", "Caldara", 33);
-console.log(other.getBio());
+// console.log(other.getBio());
